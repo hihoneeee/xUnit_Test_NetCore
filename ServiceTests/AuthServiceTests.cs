a1148b1977fbb1836a269bc5207eaf7caea5f116
hihoneeee <hohuy12344@gmail.com>
2024-07-12 17:01:15 +0700

    fix xUnitTest Login, Register

--- ServiceTests/AuthServiceTests.cs
+++ ServiceTests/AuthServiceTests.cs
@@ -64,23 +64,23 @@ namespace XUnitTestWebAPI.ServiceTests
         public async Task Register_EmailAlreadyExists_ReturnError()
         {
             // Arrange
-            var authRegisterDTO = new AuthRegisterDTO { email = "Jorge_Turcotte@gmail.com", password = "stringst", roleCode = "D22MD2", first_name = "Test", last_name = "test" };
-            _authRepoMock.Setup(repo => repo.getByEmail(authRegisterDTO.email)).ReturnsAsync(new User { first_name = authRegisterDTO.first_name, last_name = authRegisterDTO.last_name, email = authRegisterDTO.email, password = authRegisterDTO.password, roleCode = authRegisterDTO.roleCode });
+            var authRegisterDTO = new AuthRegisterDTO { phone = "12345678", password = "stringst", roleCode = "D22MD2", first_name = "Test", last_name = "test" };
+            _authRepoMock.Setup(repo => repo.getByPhoneAsync(authRegisterDTO.phone)).ReturnsAsync((User)null);
 
             // Act
             var result = await _authServices.Register(authRegisterDTO);
 
             // Assert
             Assert.False(result.success);
-            Assert.Equal("Email already exists!", result.message);
+            Assert.Equal("Phone already exists!", result.message);
         }
 
         [Fact]
         public async Task Register_RoleNotFound_ReturnError()
         {
             // Arrange
-            var authRegisterDTO = new AuthRegisterDTO { email = "test@example.com", password = "stringst", roleCode = "invalidRole", first_name = "Test", last_name = "test" };
-            _authRepoMock.Setup(repo => repo.getByEmail(authRegisterDTO.email)).ReturnsAsync((User)null);
+            var authRegisterDTO = new AuthRegisterDTO { phone = "12345678", password = "stringst", roleCode = "Invalid", first_name = "Test", last_name = "test" };
+            _authRepoMock.Setup(repo => repo.getByPhoneAsync(authRegisterDTO.phone)).ReturnsAsync((User)null); // trả về null ở đây sẽ giả dụ không có
             _roleRepoMock.Setup(repo => repo.GetRoleByCodeAsyn(authRegisterDTO.roleCode)).ReturnsAsync((Role)null);
 
             // Act
@@ -95,10 +95,12 @@ namespace XUnitTestWebAPI.ServiceTests
         public async Task Register_Success_ReturnSuccess()
         {
             // Arrange
-            var authRegisterDTO = new AuthRegisterDTO { email = "hohuy12344@gmail.com", password = "stringst", roleCode = "D22MD2", first_name = "Test", last_name = "test" };
-            _authRepoMock.Setup(repo => repo.getByEmail(authRegisterDTO.email)).ReturnsAsync((User)null);
-            _roleRepoMock.Setup(repo => repo.GetRoleByCodeAsyn(authRegisterDTO.roleCode)).ReturnsAsync(new Role { value = "Admin", code = authRegisterDTO.roleCode });
-            _authRepoMock.Setup(repo => repo.Register(It.IsAny<User>())).ReturnsAsync(new User { first_name = authRegisterDTO.first_name, last_name = authRegisterDTO.last_name, email = authRegisterDTO.email, password = authRegisterDTO.password, roleCode = authRegisterDTO.roleCode });
+            var user = new User { phone = "0922712006", password = "stringst", first_name = "string", last_name = "string", roleCode = "D22MD2" };
+            var role = new Role { value = "Admin", code = "D22MD2" };
+            var authRegisterDTO = new AuthRegisterDTO { phone = "0922712006", password = "stringst", roleCode = "D22MD2", first_name = "string", last_name = "string" };
+            _authRepoMock.Setup(repo => repo.getByPhoneAsync(authRegisterDTO.phone)).ReturnsAsync((User)null);
+            _roleRepoMock.Setup(repo => repo.GetRoleByCodeAsyn(authRegisterDTO.roleCode)).ReturnsAsync(role);
+            _authRepoMock.Setup(repo => repo.Register(It.IsAny<User>())).ReturnsAsync(user);
 
             // Act
             var result = await _authServices.Register(authRegisterDTO);
@@ -111,8 +113,8 @@ namespace XUnitTestWebAPI.ServiceTests
         public async Task Login_EmailNotFound_ReturnError()
         {
             // Arrange
-            var authLoginDTO = new AuthLoginDTO { email = "hohuy12344@gmail.com" };
-            _authRepoMock.Setup(repo => repo.getByEmail(authLoginDTO.email)).ReturnsAsync((User)null);
+            var authLoginDTO = new AuthLoginDTO { phone = "0922712006" };
+            _authRepoMock.Setup(repo => repo.getByEmail(authLoginDTO.phone)).ReturnsAsync((User)null);
 
             // Act
             var result = await _authServices.Login(authLoginDTO);
@@ -127,10 +129,10 @@ namespace XUnitTestWebAPI.ServiceTests
         public async Task Login_PasswordUnauthorized_ReturnsError()
         {
             // Assert
-            var existingEmail = new User { email = "hohuy12344@gmail.com", password = "stringst", first_name = "string", last_name = "string", roleCode = "D22MD2" };
-            var authLoginDTO = new AuthLoginDTO { email = "hohuy12344@gmail.com", password = "wrong_password" };
+            var existingEmail = new User { phone = "0922712006", password = "stringst", first_name = "string", last_name = "string", roleCode = "D22MD2" };
+            var authLoginDTO = new AuthLoginDTO { phone = "0922712006", password = "wrong_password" };
 
-            _authRepoMock.Setup(repo => repo.getByEmail(authLoginDTO.email)).ReturnsAsync(existingEmail);
+            _authRepoMock.Setup(repo => repo.getByEmail(authLoginDTO.phone)).ReturnsAsync(existingEmail);
             _hashPasswordHelper.Setup(helper => helper.VerifyPassword(authLoginDTO.password, existingEmail.password)).Returns(false);
 
             // Act
@@ -145,10 +147,10 @@ namespace XUnitTestWebAPI.ServiceTests
         public async Task Login_Success_ReturnToken()
         {
             // Assert
-            var existingEmail = new User { email = "hohuy12344@gmail.com", password = "stringst", first_name = "string", last_name = "string", roleCode = "D22MD2" };
-            var authLoginDTO = new AuthLoginDTO { email = "hohuy12344@gmail.com", password = "stringst" };
+            var existingEmail = new User { phone = "0922712006", password = "stringst", first_name = "string", last_name = "string", roleCode = "D22MD2" };
+            var authLoginDTO = new AuthLoginDTO { phone = "0922712006", password = "stringst" };
 
-            _authRepoMock.Setup(repo => repo.getByEmail(authLoginDTO.email)).ReturnsAsync(existingEmail);
+            _authRepoMock.Setup(repo => repo.getByEmail(authLoginDTO.phone)).ReturnsAsync(existingEmail);
             _hashPasswordHelper.Setup(helper => helper.VerifyPassword(authLoginDTO.password, existingEmail.password)).Returns(true);
             _jwtHelperMock.Setup(jwt => jwt.GenerateJWTToken(existingEmail.id, existingEmail.roleCode, It.IsAny<DateTime>())).ReturnsAsync("fake_jwt_token");
             _jwtHelperMock.Setup(jwt => jwt.GenerateJWTRefreshToken(existingEmail.id, existingEmail.roleCode, It.IsAny<DateTime>())).ReturnsAsync("fake_refresh_token");
